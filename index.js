f9033589bbdb05d9494dff87625b054952e0363d
Brett Seifried <seifriedbrett@gmail.com>
2022-04-05 10:36:09 -0700

    Stacking up good code

---
+++ index.js
@@ -0,0 +1,34 @@
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(item) {
+    return this.stack.push(item);
+  }
+
+  pop() {
+    return this.stack.pop() || null;
+  }
+
+  peek() {
+    return this.stack[this.length - 1];
+  }
+
+  get length() {
+    return this.stack.length;
+  }
+}
+
+const stack = new Stack();
+stack.push('fox');
+stack.push('goose');
+stack.push('lizard');
+console.log(stack.pop()); // 'lizard'
+console.log(stack.peek()); // 'goose'
+console.log(stack.pop()); // 'goose'
+stack.push('llama');
+console.log(stack.pop()); // 'llama'
+console.log(stack.peek()); // 'fox'
+console.log(stack.pop()); // 'fox'
+console.log(stack.pop()); // null
